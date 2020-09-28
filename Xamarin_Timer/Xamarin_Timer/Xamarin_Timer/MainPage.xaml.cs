e95b7b69aee27005d441a25877d497f493ab6246
castillocarlosr <castillocarlosr@gmail.com>
2020-09-28 15:35:43 -0700

    added counter to cs partial class

--- Xamarin_Timer/Xamarin_Timer/Xamarin_Timer/MainPage.xaml.cs
+++ Xamarin_Timer/Xamarin_Timer/Xamarin_Timer/MainPage.xaml.cs
@@ -10,9 +10,19 @@ namespace Xamarin_Timer
 {
     public partial class MainPage : ContentPage
     {
+        private int count = 0;
+
+        public string readme = "Howdy.  Hope you like my code here.  Love - Carlos Castillo Sept28, 2020";
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void IncrementCounterClicked(object sender, EventArgs evA)
+        {
+            count++;
+            CounterLabel.Text = count.ToString();
         }
     }
 }
